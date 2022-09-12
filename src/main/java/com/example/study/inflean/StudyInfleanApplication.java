96553056f918e2ab40e9759df42dace793fef2a4
sangwoo kim <wang9ri@gmail.com>
2022-09-12 21:54:19 +0900

    init

---
+++ src/main/java/com/example/study/inflean/StudyInfleanApplication.java
@@ -0,0 +1,13 @@
+package com.example.study.inflean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyInfleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyInfleanApplication.class, args);
+    }
+
+}
